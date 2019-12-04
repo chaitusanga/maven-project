3c0659933f5d6abc29cb7338ad4d4252da60e697
Chaitanya Sanga <csanga@HYD-LAP-00291.local>
2019-12-04 12:50:28 +0530

    checkstyle warnings fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-	* @param Chaitanya
-	* @return greeting string
+   * @param Chaitanya
+   * @return greeting string
   */
 
   public final String greet(final String someone) {
